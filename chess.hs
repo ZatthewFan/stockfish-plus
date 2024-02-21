14a46ba26009347c7f284dca00694803d29ecf9d
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2024-02-21 08:44:39 -0800

    added empty board; fixed ordering of initialBoard

--- chess.hs
+++ chess.hs
@@ -11,6 +11,42 @@ data Board = Board [(Square, Maybe Piece)] deriving (Show, Eq)
 data Pos = Pos Char Int deriving (Show, Eq)
 data State = State -- TODO
 
+emptyBoard :: Board
+emptyBoard = Board [
+    (Square 'A' 1, Nothing)),
+    (Square 'B' 1, Nothing)),
+    (Square 'C' 1, Nothing)),
+    (Square 'D' 1, Nothing)),
+    (Square 'E' 1, Nothing)),
+    (Square 'F' 1, Nothing)),
+    (Square 'G' 1, Nothing)),
+    (Square 'H' 1, Nothing)),
+    (Square 'A' 2, Nothing)),
+    (Square 'B' 2, Nothing)),
+    (Square 'C' 2, Nothing)),
+    (Square 'D' 2, Nothing)),
+    (Square 'E' 2, Nothing)),
+    (Square 'F' 2, Nothing)),
+    (Square 'G' 2, Nothing)),
+    (Square 'H' 2, Nothing)),
+    (Square 'A' 7, Nothing)),
+    (Square 'B' 7, Nothing)),
+    (Square 'C' 7, Nothing)),
+    (Square 'D' 7, Nothing)),
+    (Square 'E' 7, Nothing)),
+    (Square 'F' 7, Nothing)),
+    (Square 'G' 7, Nothing)),
+    (Square 'H' 7, Nothing))
+    (Square 'A' 8, Nothing)),
+    (Square 'B' 8, Nothing)),
+    (Square 'C' 8, Nothing)),
+    (Square 'D' 8, Nothing)),
+    (Square 'E' 8, Nothing)),
+    (Square 'F' 8, Nothing)),
+    (Square 'G' 8, Nothing)),
+    (Square 'H' 8, Nothing)),
+]
+
 initialBoard :: Board
 initialBoard = Board [
     (Square 'A' 1, Just (Piece White Rook)),
@@ -29,14 +65,6 @@ initialBoard = Board [
     (Square 'F' 2, Just (Piece White Pawn)),
     (Square 'G' 2, Just (Piece White Pawn)),
     (Square 'H' 2, Just (Piece White Pawn)),
-    (Square 'A' 8, Just (Piece Black Rook)),
-    (Square 'B' 8, Just (Piece Black Knight)),
-    (Square 'C' 8, Just (Piece Black Bishop)),
-    (Square 'D' 8, Just (Piece Black Queen)),
-    (Square 'E' 8, Just (Piece Black King)),
-    (Square 'F' 8, Just (Piece Black Bishop)),
-    (Square 'G' 8, Just (Piece Black Knight)),
-    (Square 'H' 8, Just (Piece Black Rook)),
     (Square 'A' 7, Just (Piece Black Pawn)),
     (Square 'B' 7, Just (Piece Black Pawn)),
     (Square 'C' 7, Just (Piece Black Pawn)),
@@ -45,6 +73,14 @@ initialBoard = Board [
     (Square 'F' 7, Just (Piece Black Pawn)),
     (Square 'G' 7, Just (Piece Black Pawn)),
     (Square 'H' 7, Just (Piece Black Pawn))
+    (Square 'A' 8, Just (Piece Black Rook)),
+    (Square 'B' 8, Just (Piece Black Knight)),
+    (Square 'C' 8, Just (Piece Black Bishop)),
+    (Square 'D' 8, Just (Piece Black Queen)),
+    (Square 'E' 8, Just (Piece Black King)),
+    (Square 'F' 8, Just (Piece Black Bishop)),
+    (Square 'G' 8, Just (Piece Black Knight)),
+    (Square 'H' 8, Just (Piece Black Rook)),
   ]
 
 prettyBoard :: Board -> String
