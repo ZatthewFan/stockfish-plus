d39c48956e296399fc78ba02a47693a48813f278
jerry <jerry.jge@gmail.com>
2024-02-22 13:23:57 -0800

    updated datatypes

--- chess.hs
+++ chess.hs
@@ -6,45 +6,45 @@ data Piece = Piece PieceColor PieceType deriving (Show, Eq)
 data PieceColor = White | Black deriving (Show, Eq)
 data PieceType = Pawn | Bishop | Knight | Rook | Queen | King deriving (Show, Eq)
 
-data Square = Square Char Int deriving (Show, Eq)
-data Board = Board [(Square, Maybe Piece)] deriving (Show, Eq)
 data Pos = Pos Char Int deriving (Show, Eq)
-data State = State -- TODO
+data Square = Square Pos (Maybe Piece) deriving (Show, Eq)
+data Board = Board [Square] deriving (Show, Eq)
+data State = State { gameState :: Board } deriving (Show, Eq)
 
 initialBoard :: Board
 initialBoard = Board [
-    (Square 'A' 1, Just (Piece White Rook)),
-    (Square 'B' 1, Just (Piece White Knight)),
-    (Square 'C' 1, Just (Piece White Bishop)),
-    (Square 'D' 1, Just (Piece White Queen)),
-    (Square 'E' 1, Just (Piece White King)),
-    (Square 'F' 1, Just (Piece White Bishop)),
-    (Square 'G' 1, Just (Piece White Knight)),
-    (Square 'H' 1, Just (Piece White Rook)),
-    (Square 'A' 2, Just (Piece White Pawn)),
-    (Square 'B' 2, Just (Piece White Pawn)),
-    (Square 'C' 2, Just (Piece White Pawn)),
-    (Square 'D' 2, Just (Piece White Pawn)),
-    (Square 'E' 2, Just (Piece White Pawn)),
-    (Square 'F' 2, Just (Piece White Pawn)),
-    (Square 'G' 2, Just (Piece White Pawn)),
-    (Square 'H' 2, Just (Piece White Pawn)),
-    (Square 'A' 8, Just (Piece Black Rook)),
-    (Square 'B' 8, Just (Piece Black Knight)),
-    (Square 'C' 8, Just (Piece Black Bishop)),
-    (Square 'D' 8, Just (Piece Black Queen)),
-    (Square 'E' 8, Just (Piece Black King)),
-    (Square 'F' 8, Just (Piece Black Bishop)),
-    (Square 'G' 8, Just (Piece Black Knight)),
-    (Square 'H' 8, Just (Piece Black Rook)),
-    (Square 'A' 7, Just (Piece Black Pawn)),
-    (Square 'B' 7, Just (Piece Black Pawn)),
-    (Square 'C' 7, Just (Piece Black Pawn)),
-    (Square 'D' 7, Just (Piece Black Pawn)),
-    (Square 'E' 7, Just (Piece Black Pawn)),
-    (Square 'F' 7, Just (Piece Black Pawn)),
-    (Square 'G' 7, Just (Piece Black Pawn)),
-    (Square 'H' 7, Just (Piece Black Pawn))
+    Square (Pos 'A' 1) (Just (Piece White Rook)),
+    Square (Pos 'B' 1) (Just(Piece White Knight)),
+    Square (Pos 'C' 1) (Just (Piece White Bishop)),
+    Square (Pos 'D' 1) (Just (Piece White Queen)),
+    Square (Pos 'E' 1) (Just (Piece White King)),
+    Square (Pos 'F' 1) (Just (Piece White Bishop)),
+    Square (Pos 'G' 1) (Just (Piece White Knight)),
+    Square (Pos 'H' 1) (Just (Piece White Rook)),
+    Square (Pos 'A' 2) (Just (Piece White Pawn)),
+    Square (Pos 'B' 2) (Just (Piece White Pawn)),
+    Square (Pos 'C' 2) (Just (Piece White Pawn)),
+    Square (Pos 'D' 2) (Just (Piece White Pawn)),
+    Square (Pos 'E' 2) (Just (Piece White Pawn)),
+    Square (Pos 'F' 2) (Just (Piece White Pawn)),
+    Square (Pos 'G' 2) (Just (Piece White Pawn)),
+    Square (Pos 'H' 2) (Just (Piece White Pawn)),
+    Square (Pos 'A' 8) (Just (Piece Black Rook)),
+    Square (Pos 'B' 8) (Just (Piece Black Knight)),
+    Square (Pos 'C' 8) (Just (Piece Black Bishop)),
+    Square (Pos 'D' 8) (Just (Piece Black Queen)),
+    Square (Pos 'E' 8) (Just (Piece Black King)),
+    Square (Pos 'F' 8) (Just (Piece Black Bishop)),
+    Square (Pos 'G' 8) (Just (Piece Black Knight)),
+    Square (Pos 'H' 8) (Just (Piece Black Rook)),
+    Square (Pos 'A' 7) (Just (Piece Black Pawn)),
+    Square (Pos 'B' 7) (Just (Piece Black Pawn)),
+    Square (Pos 'C' 7) (Just (Piece Black Pawn)),
+    Square (Pos 'D' 7) (Just (Piece Black Pawn)),
+    Square (Pos 'E' 7) (Just (Piece Black Pawn)),
+    Square (Pos 'F' 7) (Just (Piece Black Pawn)),
+    Square (Pos 'G' 7) (Just (Piece Black Pawn)),
+    Square (Pos 'H' 7) (Just (Piece Black Pawn))
   ]
 
 -- prettyBoard :: Board -> String
@@ -54,9 +54,9 @@ initialBoard = Board [
 evalBoard :: Board -> Int
 evalBoard (Board squares) = sum $ map valueSquare squares
     where
-        valueSquare :: (Square, Maybe Piece) -> Int
-        valueSquare (_, Nothing) = 0
-        valueSquare (_, Just piece) = valuePiece piece
+        valueSquare :: Square -> Int
+        valueSquare (Square _ Nothing) = 0
+        valueSquare (Square _ (Just piece)) = valuePiece piece
 
 -- Can also try guards instead of case
 valuePiece :: Piece -> Int
@@ -79,3 +79,7 @@ valuePiece (Piece Black pieceType) =
     Rook -> -5
     Queen -> -9
     King -> -1000
+
+-- TASK 2 --
+
+-- movePos :: Pos -> Pos -> Board -> Board
